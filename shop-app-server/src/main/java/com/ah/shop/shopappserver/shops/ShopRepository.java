897da60a125a6d388b743b3af06d74a7699745b3
Abdelalim <abdelalim.hankafi@gmail.com>
2018-02-01 15:58:41 +0000

    'Add_nearbyshops_api_with_hard_coded_location'

--- shop-app-server/src/main/java/com/ah/shop/shopappserver/shops/ShopRepository.java
+++ shop-app-server/src/main/java/com/ah/shop/shopappserver/shops/ShopRepository.java
@@ -1,9 +1,15 @@
 package com.ah.shop.shopappserver.shops;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+
 
 
 public interface ShopRepository  extends MongoRepository<Shop, ObjectId>{
   
+	public GeoResults<Shop>  findByLocationNear(Point p, Distance d); 
 }
---
+++ shop-app-server/src/main/java/com/ah/shop/shopappserver/shops/ShopResources.java
@@ -0,0 +1,38 @@
+package com.ah.shop.shopappserver.shops;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Point;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class ShopResources {
+	@Autowired
+	private ShopRepository shopRepository;
+	
+	@RequestMapping("/shops")
+	 public List<Shop> getAllShops(){
+		
+		return shopRepository.findAll();
+		 
+	 }
+	
+	@RequestMapping("/nearbyshops")
+	 public GeoResults<Shop> getNearShops(){
+		
+	
+		Point p = new Point(-6.81134,33.95564);
+		Distance d = new Distance(0.5, Metrics.KILOMETERS);
+		 
+		return shopRepository.findByLocationNear(p, d);
+		 
+	 }
+}
