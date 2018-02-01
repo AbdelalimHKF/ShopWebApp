7303292dbfda1421973f5ba918862c235f60ca3a
Abdelalim <abdelalim.hankafi@gmail.com>
2018-02-01 14:08:11 +0000

    Initial commit

---
+++ shop-app-server/src/main/java/com/ah/shop/shopappserver/ShopAppServerApplication.java
@@ -0,0 +1,12 @@
+package com.ah.shop.shopappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAppServerApplication.class, args);
+	}
+}
---
+++ shop-app-server/src/test/java/com/ah/shop/shopappserver/ShopAppServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ah.shop.shopappserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopAppServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
